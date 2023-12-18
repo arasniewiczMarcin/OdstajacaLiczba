22b787f83d9f686dbf140c9066e3287a49b50056
arasniewiczMarcin <arasniewicz.marcin@gmail.com>
2023-12-18 20:11:08 +0100

    Added function that returns wanted number

---
+++ OdstajacaLiczba/src/components/Input.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const inputArray = ref('')
+
+const findWantedNumber = (odd: number, numArray:number[]) =>{
+  
+  const wantedNumber = numArray.find((num) => num % 2 == odd)
+  console.log(wantedNumber)
+  return wantedNumber
+}
+
+const isWantedNumberOdd = (firstThreeNumArray:number[]) => {
+  let oddCount = 0
+    firstThreeNumArray.forEach((num)=>{
+    if(num % 2 == 1) oddCount++
+  })
+  if(oddCount < 2) return true
+  return false
+}
+
+const findDifferentNumber = (numArray: number[]) => {
+  if(numArray.length < 3) return "Array does not contain 3 numbers"
+   //sprawdzenie 3 pierwszych liczb
+  const firstThree = [numArray[0], numArray[1], numArray[2]]
+  
+  if(isWantedNumberOdd(firstThree)) return findWantedNumber(1, numArray)
+  else return findWantedNumber(0, numArray)
+  
+}
+
+</script>
+
+<template>
+  
+  <input v-model="inputArray" type="text" class=" border-2 border-gray-300 p-2 rounded-md w-1/2" />
+  <button class="border-2 border-gray-300 p-2 rounded-md w-1/6" @click="findDifferentNumber(inputArray.split(',').map((num)=>parseInt(num)))">Wyszukaj</button>
+  
+</template>
+
+<style scoped>
+</style>
---
+++ OdstajacaLiczba/src/features/main-screen.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import Input from '../components/Input.vue'
+
+
+</script>
+
+<template>
+    <div class="flex flex-col gap-8 items-center m-8">
+        <Input />
+    </div>
+</template>
+
+<style scoped>
+</style>
